e08ed18a3d570d96875d3f55ca055591a4b9ec8a
nadyacherepaha <nad.cherepaha@gmail.com>
2021-09-15 19:32:47 +0300

    Add description for commands

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ const token = '1970300638:AAH_6XTXV9m811EwTNSsaJw9uhijvQm8144'
 
 const bot = new TelegramApi(token, {polling: true})
 
-bot.on('message', async msg => {
-  const text = msg.text
-  const chatId = msg.chat.id
+const start = () => {
+  bot.setMyCommands([
+    {command: '/start', description: 'Начальное приветствие'},
+    {command: '/info', description: 'Информация о боте'}
+  ])
 
-  if (text === '/start') {
-    await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/88e/586/88e586f0-4299-313f-bedb-ef45c7710422/1.webp')
-    await bot.sendMessage(chatId, `Добро пожаловать в телеграм бот, ${msg.from.first_name}`)
-  }
-  if (text === '/info') {
-    await bot.sendMessage(chatId, 'Этот тестовый бот создан для того, чтобы поиграть с тобой в игру')
-  }
-})
+  bot.on('message', async msg => {
+    const text = msg.text
+    const chatId = msg.chat.id
+
+    if (text === '/start') {
+      await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/88e/586/88e586f0-4299-313f-bedb-ef45c7710422/1.webp')
+      await bot.sendMessage(chatId, `Добро пожаловать в телеграм бот, ${msg.from.first_name}`)
+    }
+    if (text === '/info') {
+      await bot.sendMessage(chatId, 'Этот тестовый бот создан для того, чтобы поиграть с тобой в игру')
+    }
+  })
+}
+
+start()
