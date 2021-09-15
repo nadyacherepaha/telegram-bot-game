7e3a1c627b1bb1274e8c287b2d26ad49e5d8fe9a
nadyacherepaha <nad.cherepaha@gmail.com>
2021-09-15 19:09:53 +0300

    Add a sticker to the start command

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const token = '1970300638:AAH_6XTXV9m811EwTNSsaJw9uhijvQm8144'
 
 const bot = new TelegramApi(token, {polling: true})
 
-bot.on('message', msg => {
+bot.on('message', async msg => {
   const text = msg.text
   const chatId = msg.chat.id
 
   if (text === '/start') {
-    bot.sendMessage(chatId, `Добро пожаловать в телеграм бот, ${msg.from.first_name}`)
+    await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/88e/586/88e586f0-4299-313f-bedb-ef45c7710422/1.webp')
+    await bot.sendMessage(chatId, `Добро пожаловать в телеграм бот, ${msg.from.first_name}`)
   }
   if (text === '/info') {
-    bot.sendMessage(chatId, 'Этот тестовый бот создан для того, чтобы поиграть с тобой в игру')
+    await bot.sendMessage(chatId, 'Этот тестовый бот создан для того, чтобы поиграть с тобой в игру')
   }
 })
